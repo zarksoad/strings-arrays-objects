8c2be415a9f0ce2539c7be0310c27598d1e17c59
Hernan <110841804+zarksoad@users.noreply.github.com>
2024-04-22 09:57:19 -0500

    Secttio 2 first challange done

---
+++ section 2/challange1.js
@@ -0,0 +1,7 @@
+// average grades 
+
+let grades = prompt("Enter the grades separate by ','").split(",")
+
+const average = grades.reduce((acummalator,element) =>(acummalator +element /grades.length),0)
+
+alert(`the average grades is ${average}`)
