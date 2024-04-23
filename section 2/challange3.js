348e94f6be3d3d9e5c5fc57d405a43dd77f6dd87
JuanSebastian <juan.1999@hotmail.es>
2024-04-23 17:33:53 -0500

    Third challange in process delete.

--- section 2/challange3.js
+++ section 2/challange3.js
@@ -30,107 +30,119 @@ function duplicate(list) {
         let newName = `${search.name} copy` // lampara 
         let counterDuplicate = 1
 
-        while(list.some(item => item.name === newName)){
+        while (list.some(item => item.name === newName)) {
             newName = `${search.name} copy (${counterDuplicate})`;
             counterDuplicate++;
         }
-        
+
         list.push({
+            ...search,
             id: counter++, name: newName,
-            price: search.price,
-            quantity: search.quantity,
-            description: search.description
         })
-    }}
-function checkInventory(list){
+    }
+}
+function checkInventory(list) {
     console.table(list)
     let checkNombre = prompt("Enter the name of the item")
     let searchName = list.filter(item => item.name.includes(checkNombre))
-    if(searchName.length >0){
+    if (searchName.length > 0) {
         console.table(searchName)
     }
-    else{
+    else {
         alert("the item there is not in the list")
     }
     let checkByCostMin = parseInt(prompt("Enter the cost the range of the cost min :"))
     let checkByCostMax = parseInt(prompt("Enter the cost the range of the cost max "))
-    if(!isNaN(checkByCostMax) && !isNaN(checkByCostMin) ){
+    if (!isNaN(checkByCostMax) && !isNaN(checkByCostMin)) {
         let range = list.filter(item => item.cost >= checkByCostMin && item.cost <= checkByCostMax)
-        if(range.length === 0){
+        if (range.length === 0) {
             alert("There are not articles with this range of price")
         }
-        else{
-             console.table(range)
-    }
+        else {
+            console.table(range)
+        }
     }
 }
-function updateIventary(list){
+function updateIventary(list) {
     console.table(list)
     let searchById = parseInt(prompt("Enter the Item that you need to change by Id"))
     let flag = this
-    if(!isNaN(searchById)){
+    if (!isNaN(searchById)) {
         let findId = list.find(item => item.id === searchById)
-        if(findId){
+        if (findId) {
             console.table(findId)
-            while(flag){
+            while (flag) {
                 flag = confirm("Would you like to continue modifyng properties?")
-                if(!flag)break;
+                if (!flag) break;
                 let menu = parseInt(prompt("Enter the opcion to modify \n 1 to name \n 2 to price  \n 3 to quantity  \n 4 to description"))
-                if(!isNaN(menu)){
-                    if(menu === 1){
+                if (!isNaN(menu)) {
+                    if (menu === 1) {
                         let newName = prompt("Enter the new name of the product")
                         findId.name = newName
-                        
+
                     }
-                    else if (menu === 2){
+                    else if (menu === 2) {
                         let newPrice = parseFloat(prompt("Enter the new price of the product"))
-                        if(!isNaN(newPrice)){
+                        if (!isNaN(newPrice)) {
                             findId.price = newPrice
                             console.table(findId)
                         }
-                        else{
+                        else {
                             alert("Enter a valid price")
                         }
                     }
-                    else if (menu === 3){
+                    else if (menu === 3) {
                         let newQuantity = parseFloat(prompt("Enter the new Quantity of the product"))
-                        if(!isNaN(newQuantity)){
+                        if (!isNaN(newQuantity)) {
                             findId.quantity = newQuantity
                             console.table(findId)
                         }
-                        else{
+                        else {
                             alert("Enter a valid quantity")
                         }
 
                     }
-                    else if(menu === 4){
+                    else if (menu === 4) {
                         let newDescription = prompt("Enter the new description of the product")
                         findId.description = newDescription
-                        
+
                     }
-                    else{
+                    else {
                         alert("Enter a valid opcion")
                     }
                 }
-                else{
+                else {
                     alert("Enter a valid opcion")
                 }
-                    
-                }
-            console.table(list)    
+
+            }
+            console.table(list)
 
         }
-        else{
+        else {
             alert("The ID doesn't exist in stock")
         }
-        
+
     }
-    else{
+    else {
         alert("Please enter a valid Id")
     }
-    
 
-    
+
+
+}
+
+function deleteElement(list) {
+    let searchById = parseInt(prompt("Enter the Item that you need to change by Id"))
+    if (!isNaN(searchById)) {
+        opcion = confirm(`Estas seguro que deseas eliminar el producto con sus propiedad ${searchById}?`)
+        if (opcion == true) {
+            let_new_list = list.filter((item) => item.id !== searchById)
+        }
+        else {
+            return
+        }
+    }
 }
 // for test
 const listaObjetos = [
@@ -144,9 +156,5 @@ const listaObjetos = [
     { id: counter++, name: "cojin", price: 12.99, quantity: 25, description: "Cojín decorativo con estampado floral." }
 ]
 
-duplicate(listaObjetos)
-duplicate(listaObjetos)
-updateIventary(listaObjetos)
-console.table(listaObjetos)
-
-
+deleteElement(listaObjetos)
+console.table(listaObjetos);
